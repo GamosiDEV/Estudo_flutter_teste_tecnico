5b6ca24f250bfa9a2055c5eade4b775159dfb180
GamosiDEV <gm.silva111297@gmail.com>
2022-01-19 17:39:50 -0300

    Preparando para receber os dados do GIT

---
+++ lib/Model/Request.dart
@@ -0,0 +1,31 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+enum SortOrNot { stars,  notSort }
+
+class Request {
+
+  Future<Map> getRepositories(
+      String query, SortOrNot sortOrNot, {int offSet=1}) async {
+    http.Response response;
+
+    if (query == null || query == '') {
+      query = '';
+    } else {
+      query = query+ '+';
+    }
+
+    switch (sortOrNot) {
+      case SortOrNot.notSort:
+        response = await http.get(
+            'https://api.github.com/search/repositories?q=${query}language:Java&page=$offSet');
+        break;
+      case SortOrNot.stars:
+        response = await http.get(
+            'https://api.github.com/search/repositories?q=${query}language:Java&sort=stars&page=$offSet');
+        break;
+    }
+
+    return json.decode(response.body);
+  }
+}
